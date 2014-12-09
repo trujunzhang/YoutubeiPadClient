fdadc90ce4af2ddacaed19484b8dc2c418bf5312
wanghaogithub720 <wanghaomspace@gmail.com>
2014-12-09 10:16:31 +0800

    update install.sh.

--- install.sh
+++ install.sh
@@ -2,4 +2,7 @@
 
 #/Volumes/Home/Developing/SketchProjects/YoutubeiPadClient/dependencies/layers/Business-Logic-Layer/AppResource
 
-rm -rf mxAsTubeiPad&&unzip  mxAsTubeiPad.zip&&cp dependencies/layers/Business-Logic-Layer/AppResource/Podfile mxAsTubeiPad&&cp dependencies/layers/Business-Logic-Layer/AppResource/Main.storyboard mxAsTubeiPad/mxAsTubeiPad/Base.lproj&&cd mxAsTubeiPad&&pod install  --verbose 
+_appcode_alias="/Volumes/macshare/Applications/developer/IDEs/jetbeains/AppCode.app"
+
+rm -rf mxAsTubeiPad&&unzip  mxAsTubeiPad.zip&&cp dependencies/layers/Business-Logic-Layer/AppResource/Podfile mxAsTubeiPad&&cp dependencies/layers/Business-Logic-Layer/AppResource/Main.storyboard mxAsTubeiPad/mxAsTubeiPad/Base.lproj&&cd mxAsTubeiPad&&pod install  --verbose&&open -a $_appcode_alias mxAsTubeiPad.xcworkspace
+
