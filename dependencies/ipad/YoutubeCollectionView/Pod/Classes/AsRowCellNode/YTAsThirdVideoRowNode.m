ae286e57161c03d42c4da6a958741f9893d1d975
wanghaogithub720 <wanghaomspace@gmail.com>
2014-12-11 23:49:13 +0800

    convert libraries to local.

--- dependencies/ipad/YoutubeCollectionView/Pod/Classes/AsRowCellNode/YTAsThirdVideoRowNode.m
+++ dependencies/ipad/YoutubeCollectionView/Pod/Classes/AsRowCellNode/YTAsThirdVideoRowNode.m
@@ -7,7 +7,8 @@
 
 
 @interface YTAsThirdVideoRowNode () {
-
+   ASTextNode * _channelTitleNode;
+   ASDisplayNode * _divider;
 }
 @end
 
@@ -18,7 +19,31 @@
 
 
 - (void)makeRowNode {
-   self.backgroundColor = [UIColor lightGrayColor];
+   _channelTitleNode = [ASTextNode initWithAttributedString:
+    [[NSAttributedString alloc] initWithString:[YoutubeParser getVideoSnippetChannelTitle:self.nodeVideo]
+                                    attributes:[self textStyleForChannelTitle]]];
+   [self addSubnode:_channelTitleNode];
+
+   // hairline cell separator
+   _divider = [[ASDisplayNode alloc] init];
+   _divider.backgroundColor = [UIColor lightGrayColor];
+   [self addSubnode:_divider];
+}
+
+
+- (NSDictionary *)textStyleForChannelTitle {
+   UIFont * font = [UIFont fontWithName:@"HelveticaNeue" size:14.0f];
+
+   NSMutableParagraphStyle * style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+   style.paragraphSpacing = 0.5 * font.lineHeight;
+   style.hyphenationFactor = 1.0;
+   style.lineBreakMode = NSLineBreakByTruncatingTail;
+
+   return @{
+    NSFontAttributeName : font,
+    NSParagraphStyleAttributeName : style,
+    NSForegroundColorAttributeName : [UIColor lightGrayColor]
+   };
 }
 
 
@@ -28,7 +53,13 @@
 
 
 - (void)layout {
+   CGFloat titleLeftX = 2.0f;
+   CGFloat titleWidth = 180.0f;
+   _channelTitleNode.frame = CGRectMake(titleLeftX, 36, titleWidth, 20);
 
+   CGFloat pixelHeight = 1.0f / [[UIScreen mainScreen] scale];
+   _divider.frame =
+    CGRectMake(0.0f, self.cellRect.size.height - pixelHeight, self.calculatedSize.width, pixelHeight);
 }
 
 
