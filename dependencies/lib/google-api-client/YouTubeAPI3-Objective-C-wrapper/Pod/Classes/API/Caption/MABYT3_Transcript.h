0add664e452aa35738b5edd8a33c7c1352e6fc7a
wanghaogithub720 <wanghaomspace@gmail.com>
2014-12-14 22:23:27 +0800

    dev video caption track list.

---
+++ dependencies/lib/google-api-client/YouTubeAPI3-Objective-C-wrapper/Pod/Classes/API/Caption/MABYT3_Transcript.h
@@ -0,0 +1,14 @@
+//
+// Created by djzhang on 12/14/14.
+// Copyright (c) 2014 djzhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface MABYT3_Transcript : NSObject
+
+
+
+@end
---
+++ dependencies/lib/google-api-client/YouTubeAPI3-Objective-C-wrapper/Pod/Classes/API/Caption/MABYT3_Transcript.m
@@ -0,0 +1,12 @@
+//
+// Created by djzhang on 12/14/14.
+// Copyright (c) 2014 djzhang. All rights reserved.
+//
+
+#import "MABYT3_Transcript.h"
+
+
+@implementation MABYT3_Transcript {
+
+}
+@end
